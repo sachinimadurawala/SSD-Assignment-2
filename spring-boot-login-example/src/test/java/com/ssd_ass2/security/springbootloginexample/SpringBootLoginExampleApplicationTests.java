9bd39882e33d7ad4394ee441ea13440b1c098317
it17000154_sachini <it17000154@my.sliit.lk>
2020-09-26 23:19:48 +0530

    Facebook Login using OAuth 2.0

---
+++ spring-boot-login-example/src/test/java/com/ssd_ass2/security/springbootloginexample/SpringBootLoginExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssd_ass2.security.springbootloginexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLoginExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
